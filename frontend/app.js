6748e8229bdf5b2ea02831d67e6b7086a675fd40
André Luis M. Pinto <andre.luis.m.pinto@gmail.com>
2019-06-15 20:43:57 -0300

    Rev.01
    Rev.01

---
+++ frontend/app.js
@@ -0,0 +1,24 @@
+var app = angular.module("App", ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when("/",{
+        templateUrl: "'../app/server'"
+    })
+      .when("/event-list",{
+        templateUrl: "'../app/server'"
+    })
+      .when("/highlighted-events",{
+        templateUrl: "'../app/server'"
+    })
+      .when("/event-detail",{
+        templateUrl: "'../app/server'"
+    })
+      .when("/new-event",{
+        templateUrl: "'../app/server'"
+    })
+      .when("/share-event",{
+        templateUrl: "'../app/server'"
+    })
+    .otherwise({redirectTo : '/'});
+});
