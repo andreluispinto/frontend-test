dc24988b1c39e5540384c42cd132d373216354b5
André Luis M. Pinto <andre.luis.m.pinto@gmail.com>
2019-06-15 21:50:36 -0300

    Rev.02
    Rev.02

--- frontend/app.js
+++ frontend/app.js
@@ -1,24 +1,57 @@
 var app = angular.module("app", ['ngRoute']);
+var db = require('./db');
+var express = require('express');
+var bodyParser = require('body-parser')
+var cors = require('cors');
+var app = express();
 
-app.config(function($routeProvider){
-    $routeProvider
-    .when("/",{
-        templateUrl: "'../app/server'"
-    })
-      .when("/event-list",{
-        templateUrl: "'../app/server'"
-    })
-      .when("/highlighted-events",{
-        templateUrl: "'../app/server'"
-    })
-      .when("/event-detail",{
-        templateUrl: "'../app/server'"
-    })
-      .when("/new-event",{
-        templateUrl: "'../app/server'"
-    })
-      .when("/share-event",{
-        templateUrl: "'../app/server'"
-    })
-    .otherwise({redirectTo : '/'});
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/events', function (_req, res) {
+  res.json({events: db.findAll()});
 });
+
+app.get('/events/featured', function (_req, res) {
+  res.json({events: db.featured()});
+});
+
+app.get('/events/:id', function (req, res) {
+  var event = db.finById(req.params.id);
+  if(event) {
+    res.json({event: event});
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.post('/events', function (req, res) {
+  res.json({event: db.create(req.body.event)});
+});
+
+module.exports = app;
+
+
+
+//<!--app.config(function($routeProvider){
+  //  $routeProvider
+  //  .when("/",{
+  //      templateUrl: "'../app/server'"
+  //  })
+  //    .when("/event-list",{
+  //      templateUrl: "'../app/server'"
+  //  })
+  //    .when("/highlighted-events",{
+  //      templateUrl: "'../app/server'"
+  //  })
+  //    .when("/event-detail",{
+  //      templateUrl: "'../app/server'"
+  //  })
+  //    .when("/new-event",{
+  //       templateUrl: "'../app/server'"
+  //  })
+  //    .when("/share-event",{
+  //      templateUrl: "'../app/server'"
+  //  })
+  //  .otherwise({redirectTo : '/'});
+//});
